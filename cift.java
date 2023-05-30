e915da5c117630502aa0396fe2bb18c005a9c100
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-30 21:30:09 +0300

    Create cift.java

---
+++ cift.java
@@ -0,0 +1,8 @@
+public class cift {
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i+=2){ /* i yi 2 olarak belirler ve 100 e ulaşana kadar 2 ekler */
+            System.out.println(i); /* i 100 e ulaşana kadar çift sayıları yazar*/
+        }
+        System.out.println("Bitti."); /* 100 e gelindiğinde "bitti" yazdırır*/
+    }
+}
